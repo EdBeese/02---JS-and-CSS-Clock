64d0e42b8b8fab02c917bcd9017885e47822ed80
Edward Beesley <edwardjbeesley@gmail.com>
2022-07-16 15:00:03 +0100

    added function that will set the rotation based on the current time

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// This first function will move the clock around, but will always start at 12:00
 let secondStart = 90
 let minuteStart = 90
 let hourStart = 90
@@ -5,7 +6,7 @@ const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
-function setDate() {
+function tickingClock() {
   setInterval(() => {
     secondHand.style['transform'] = `rotate(${secondStart + 6}deg)`;
     secondStart = secondStart + 6;
@@ -16,4 +17,22 @@ function setDate() {
   }, 1000);
 }
 
-setDate()
+// This function sets the current time, although it does not update hour and minites until the turn over.
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  secondHand.style['transform'] = `rotate(${(seconds * 6) + 90}deg)`;
+  minuteHand.style['transform'] = `rotate(${(minutes * 6) + 90}deg)`;
+  hourHand.style['transform'] = `rotate(${(hours * 30) + 90}deg)`;
+}
+
+function setTime() {
+  setInterval(() => {
+    setDate();
+  }, 1000);
+}
+
+setTime();
